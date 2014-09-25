796614687fd85dbbeb1f1a7017a174477d2d36a8
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2014-09-25 12:17:50 +0200

    Some code cleanup on the plotting function and general python script

---
+++ python/plotDeltaEtaDeltaPhi.py
@@ -0,0 +1,73 @@
+from ROOT import ROOT,gROOT,gStyle,TCanvas,TFile,TH1D,TH2D,TLegend,THStack,TPaveText,TBox
+import sys
+import os
+import PlotStyle
+import numpy as np
+
+PlotStyle.setPlotStyle()
+gStyle.SetPalette(1)
+
+
+
+def drawHoBoxes(canvas):
+    canvas.cd()
+    boxes = []
+    for i in range(1,8):
+        for j in range(1,8):
+            box = TBox(-0.3045 + (i-1)*0.087,-0.3045 + (j-1)*0.087,-0.3045 + i*0.087,-0.3045 + j*0.087)
+            box.SetFillStyle(0)
+            box.SetLineColor(ROOT.kBlack)
+            box.SetLineWidth(1)
+            box.Draw()
+            boxes.append(box)
+    return boxes
+
+
+if(len(sys.argv) < 2):
+    print 'Error! Filename as first argument needed.'
+    sys.exit(1)
+
+if( not os.path.exists('plots')):
+    os.mkdir('plots')
+
+filename = sys.argv[1]
+print 'Opening file:',filename
+
+file = TFile.Open(filename)
+
+
+h2dDeltaEtaDeltaPhi = file.Get("hoMuonAnalyzer/etaPhi/L1MuonWithHoMatch_DeltaEtaDeltaPhi")
+
+canv = TCanvas("canvasDeltaEtaDeltaPhi",'canvasDeltaEtaDeltaPhi',1200,1200)
+h2dDeltaEtaDeltaPhi.Rebin2D(8,8)
+h2dDeltaEtaDeltaPhi.GetXaxis().SetRangeUser(-.45,.45)
+h2dDeltaEtaDeltaPhi.GetXaxis().SetTitle("#Delta#eta")
+h2dDeltaEtaDeltaPhi.GetYaxis().SetRangeUser(-.45,.45)
+h2dDeltaEtaDeltaPhi.GetYaxis().SetTitle("#Delta#phi")
+h2dDeltaEtaDeltaPhi.GetZaxis().SetTitle("N")
+h2dDeltaEtaDeltaPhi.Draw("colz")
+
+boxList = drawHoBoxes(canv)
+
+canv.Update()
+pal = h2dDeltaEtaDeltaPhi.GetListOfFunctions().FindObject("palette")
+pal.SetX2NDC(0.92)
+
+stats = h2dDeltaEtaDeltaPhi.GetListOfFunctions().FindObject("stats")
+stats.SetX1NDC(.1)
+stats.SetX2NDC(.2)
+stats.SetY1NDC(.1)
+stats.SetY2NDC(.25)
+
+legend = TLegend(0.7,0.8,0.9,0.9)
+legend.AddEntry(boxList[0],"HO tile dimensions","le")
+legend.Draw()
+
+canv.Update();
+
+canv.SaveAs("plots/DeltaEtaDeltaPhi.png")
+canv.SaveAs("plots/DeltaEtaDeltaPhi.pdf")
+
+f = TFile.Open("plots/DeltaEtaDeltaPhi.root","RECREATE")
+canv.Write()
+f.Close()
---
+++ python/plotEnergy.py
@@ -0,0 +1,57 @@
+from ROOT import ROOT,gROOT,TCanvas,TFile,TH1D,TH2D,TLegend,THStack,TPaveText
+import sys,os
+import PlotStyle
+import numpy as np
+
+if(len(sys.argv) < 2):
+    print 'Error! Filename as first argument needed.'
+    sys.exit(1)
+
+if( not os.path.exists('plots')):
+    os.mkdir('plots')
+
+filename = sys.argv[1]
+print 'Opening file:',filename
+file = TFile.Open(filename)
+
+
+#Set plot style
+PlotStyle.setPlotStyle()
+
+ho = file.Get("hoMuonAnalyzer/energy/horeco_Energy")
+L1MuonAndHoMatch = file.Get('hoMuonAnalyzer/energy/L1MuonWithHoMatch_Energy')
+L1MuonAndHoMatchAboveThr = file.Get('hoMuonAnalyzer/energy/L1MuonWithHoMatchAboveThr_Energy')
+
+canv = TCanvas("energieCanvas",'Energy canvas',1200,1200)
+canv.SetLogy()
+
+ho.SetStats(0)
+ho.SetTitle('Energy distribution of HO hits')
+ho.GetXaxis().SetTitle('Reconstructed HO energy / GeV')
+ho.GetYaxis().SetTitle('N')
+ho.GetXaxis().SetRangeUser(-0.2,4)
+
+ho.SetLineColor(ROOT.kBlack)
+L1MuonAndHoMatch.SetLineColor(ROOT.kBlue)
+L1MuonAndHoMatchAboveThr.SetLineColor(ROOT.kRed)
+
+ho.SetLineWidth(3)
+L1MuonAndHoMatch.SetLineWidth(3)
+L1MuonAndHoMatchAboveThr.SetLineWidth(3)
+
+ho.Draw()
+L1MuonAndHoMatch.Draw('same')
+L1MuonAndHoMatchAboveThr.Draw('same')
+
+legend = TLegend(0.5,0.65,0.9,0.9)
+legend.AddEntry(ho,'All HO hits','l')
+legend.AddEntry(L1MuonAndHoMatch,'L1 + HO match','l')
+legend.AddEntry(L1MuonAndHoMatchAboveThr,'L1 + HO match > 0.2 GeV','l')
+legend.Draw()
+
+canv.SaveAs("plots/Energieverteilung.png")
+canv.SaveAs("plots/Energieverteilung.pdf")
+
+f = TFile.Open("plots/Energieverteilung.root","RECREATE")
+canv.Write()
+f.Close()
