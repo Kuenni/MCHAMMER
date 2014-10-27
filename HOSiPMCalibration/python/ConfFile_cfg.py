e5a1e354925b584a1baca27c75ffb940fc140f37
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2014-10-27 13:51:08 +0100

    Edit file to get the text file calibrations for 2023 studies

--- HOSiPMCalibration/python/ConfFile_cfg.py
+++ HOSiPMCalibration/python/ConfFile_cfg.py
@@ -5,11 +5,12 @@ process = cms.Process("Demo")
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.load('Configuration.StandardSequences.Services_cff')
-process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
-process.load('Configuration.Geometry.GeometrySimDB_cff')
+#process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.Geometry.GeometryExtended2023MuonReco_cff')
+process.load('Configuration.Geometry.GeometryExtended2023Muon_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:mc', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgradePLS3', '')
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
@@ -25,3 +26,9 @@ process.demo = cms.EDAnalyzer(
 
 
 process.p = cms.Path(process.demo)
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.combinedCustoms
+from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_2023Muon 
+
+#call to customisation function cust_2023Muon imported from SLHCUpgradeSimulations.Configuration.combinedCustoms
+process = cust_2023Muon(process)
