17b666d33be5ade9fdd5c1ca370aa06529369ef5
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2014-10-31 12:04:37 +0100

    Add plotting script for the crabSiPMCalib scenario

--- 2023-Studies/crabSiPMCalib/crabSiPMCalibPlots.py
+++ 2023-Studies/crabSiPMCalib/crabSiPMCalibPlots.py
@@ -12,13 +12,16 @@ from plotEfficiency import plotEfficiency
 from plotMultiplicity import plotMultiplicity
 from plotPtCorrelation import plotPtCorrelation
 
+print 'File crabSiPMCalibPlots'
+
 filename = 'L1MuonHistogram.root'
 
 if(len(sys.argv) > 1 ):
 	filename = sys.argv[1]
 
 toProcess = [
-             'crabSiPMCalib'
+             '.'
+   #         ,'crab2023MinBias'
              ]
 
 for s in toProcess:
@@ -38,3 +41,22 @@ for s in toProcess:
 	plotMultiplicity(s)
 
 print 'All done.'
+
+c1 = plotEnergyVsEta('.','L1MuonWithHoMatch_EnergyVsEta')
+c1.SetName('c1')
+c1.SaveAs('energyVsEta_L1MuonAndHo.png')
+c2 = plotEnergyVsEta('.','L1MuonWithHoMatchAboveThr_EnergyVsEta')
+c2.SetName('c2')
+c2.SaveAs('energyVsEta_L1MuonAndHoAboveThr.png')
+c3 = plotEnergyVsEta('.','L1MuonWithHoMatchAboveThrFilt_EnergyVsEta')
+c3.SetName('c3')
+c3.SaveAs('energyVsEta_L1MuonAndHoAboveThrFilt.png')
+cPhi1 = plotEnergyVsPhi('.','L1MuonWithHoMatch_EnergyVsPhi')
+cPhi1.SetName('cPhi1')
+cPhi1.SaveAs('energyVsPhi_L1MuonAndHo.png')
+cPhi2 = plotEnergyVsPhi('.','L1MuonWithHoMatchAboveThr_EnergyVsPhi')
+cPhi2.SetName('cPhi2')
+cPhi2.SaveAs('energyVsPhi_L1MuonAndHoAboveThr.png')
+cPhi3 = plotEnergyVsPhi('.','L1MuonWithHoMatchAboveThrFilt_EnergyVsPhi')
+cPhi3.SetName('cPhi2')
+cPhi3.SaveAs('energyVsPhi_L1MuonAndHoAboveThrFilt.png')
