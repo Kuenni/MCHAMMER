d11b5f6ed1954c6472cfcc7e4c89109020ea93af
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2014-12-08 15:58:15 +0100

    Add some plotting scripts for the single muon gun study Add python config for the single muon gun production

---
+++ PostLS1/SingleMuonGun/makePlots.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+import os,sys
+
+sys.path.append(os.path.abspath("/user/kuensken/ChrisAnelliCode/CMSSW_6_2_0_SLHC11/src/HoMuonTrigger/python"))
+
+from plotEnergy import plotEnergy, plotEnergyVsEta, plotEnergyVsPhi
+from plotDeltaEtaDeltaPhi import plotDeltaEtaDeltaPhi
+from plotEtaPhi import plotEtaPhi
+from plotL1Rate import plotL1Rates
+from plotPhi import plotPhi
+from plotEfficiency import plotEfficiency
+from plotMultiplicity import plotMultiplicity
+from plotPtCorrelation import plotPtCorrelation
+
+print 'File crabSiPMCalibPlots'
+
+filename = 'L1MuonHistogram.root'
+
+if(len(sys.argv) > 1 ):
+	filename = sys.argv[1]
+
+toProcess = [
+             '.'
+   #         ,'crab2023MinBias'
+             ]
+
+for s in toProcess:
+	print 'Doing energy...'
+	plotEnergy(s)
+	print 'Doing Delta Eta Delta Phi...'
+	plotDeltaEtaDeltaPhi(s)
+	print 'Doing Eta Phi...'
+	plotEtaPhi(s)
+	print 'Doing L1Rates...'
+	plotL1Rates(s)
+# 	print 'Doing Phi...'
+# 	plotPhi(s)
+	print 'Doing Efficiency...'
+	plotEfficiency(s)
+	print 'Doing Multiplicity...'
+	plotMultiplicity(s)
+
+print 'All done.'
+
+c1 = plotEnergyVsEta('.','L1MuonWithHoMatch_EnergyVsEta')
+c1.SetName('c1')
+c1.SaveAs('energyVsEta_L1MuonAndHo.png')
+c2 = plotEnergyVsEta('.','L1MuonWithHoMatchAboveThr_EnergyVsEta')
+c2.SetName('c2')
+c2.SaveAs('energyVsEta_L1MuonAndHoAboveThr.png')
+c3 = plotEnergyVsEta('.','L1MuonWithHoMatchAboveThrFilt_EnergyVsEta')
+c3.SetName('c3')
+c3.SaveAs('energyVsEta_L1MuonAndHoAboveThrFilt.png')
+cPhi1 = plotEnergyVsPhi('.','L1MuonWithHoMatch_EnergyVsPhi')
+cPhi1.SetName('cPhi1')
+cPhi1.SaveAs('energyVsPhi_L1MuonAndHo.png')
+cPhi2 = plotEnergyVsPhi('.','L1MuonWithHoMatchAboveThr_EnergyVsPhi')
+cPhi2.SetName('cPhi2')
+cPhi2.SaveAs('energyVsPhi_L1MuonAndHoAboveThr.png')
+cPhi3 = plotEnergyVsPhi('.','L1MuonWithHoMatchAboveThrFilt_EnergyVsPhi')
+cPhi3.SetName('cPhi2')
+cPhi3.SaveAs('energyVsPhi_L1MuonAndHoAboveThrFilt.png')
---
+++ Production/crabMuonGunPostLS1/SingleMuPt100_cfi_GEN_SIM_DIGI_DIGI2RAW_RAW2DIGI_L1_L1Reco.py
@@ -0,0 +1,125 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: SingleMuPt100_cfi -s GEN,SIM,DIGI:pdigi_valid,DIGI2RAW,RAW2DIGI,L1,L1Reco --mc --conditions POSTLS171_V15::All --magField 38T_PostLS1 --eventcontent FEVTDEBUGHLT -n 1000 --no_exec --geometry Extended2015 --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('L1Reco')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2015_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+process.load('Configuration.StandardSequences.RawToDigi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.L1Reco_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.19 $'),
+    annotation = cms.untracked.string('SingleMuPt100_cfi nevts:1000'),
+    name = cms.untracked.string('Applications')
+)
+
+# Output definition
+
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(1048576),
+    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+    fileName = cms.untracked.string('SingleMuPt100_cfi_GEN_SIM_DIGI_DIGI2RAW_RAW2DIGI_L1_L1Reco.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+process.mix.digitizers = cms.PSet(process.theDigitizersValid)
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'POSTLS171_V15::All', '')
+
+process.generator = cms.EDProducer("FlatRandomPtGunProducer",
+    PGunParameters = cms.PSet(
+        MaxPt = cms.double(100.01),
+        MinPt = cms.double(99.99),
+        PartID = cms.vint32(-13),
+        MaxEta = cms.double(0.8),
+        MaxPhi = cms.double(3.14159265359),
+        MinEta = cms.double(-0.8),
+        MinPhi = cms.double(-3.14159265359)
+    ),
+    Verbosity = cms.untracked.int32(0),
+    psethack = cms.string('single mu pt 100'),
+    AddAntiParticle = cms.bool(False),
+    firstRun = cms.untracked.uint32(1)
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi_valid)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.raw2digi_step = cms.Path(process.RawToDigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.L1Reco_step = cms.Path(process.L1Reco)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step
+							,process.genfiltersummary_step
+							,process.simulation_step
+							,process.digitisation_step
+							,process.L1simulation_step
+							,process.digi2raw_step
+							,process.raw2digi_step
+							,process.L1Reco_step
+							,process.endjob_step
+							,process.FEVTDEBUGHLToutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
+from SLHCUpgradeSimulations.Configuration.postLS1Customs import customisePostLS1 
+
+#call to customisation function customisePostLS1 imported from SLHCUpgradeSimulations.Configuration.postLS1Customs
+process = customisePostLS1(process)
+
+# End of customisation functions
