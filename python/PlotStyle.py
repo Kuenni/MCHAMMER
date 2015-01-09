801ed70ab4e1b9d52d597137c7e10b4ebf1294a0
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-01-09 16:17:07 +0100

    Add RWTH Colors to plot style and a standard label for cms private simulation work

--- python/PlotStyle.py
+++ python/PlotStyle.py
@@ -1,4 +1,12 @@
-from ROOT import gROOT,gStyle
+from ROOT import gROOT,gStyle, TColor, TPaveText
+
+colorRwthMagenta 	= TColor.GetColor("#E30066")
+colorRwthLightBlue 	= TColor.GetColor("#8EBAE5")
+colorRwthDarkBlue 	= TColor.GetColor("#00549F")
+
+labelCmsPrivateSimulation = TPaveText(0.1,0.901,0.4,0.93,"NDC")
+labelCmsPrivateSimulation.AddText("#font[62]{CMS private}, #font[72]{simulation}")
+labelCmsPrivateSimulation.SetBorderSize(1)
 
 def setPlotStyle():
     gStyle.SetPadGridX(1)
@@ -11,3 +19,4 @@ def setPlotStyle():
     gStyle.SetTextFont(62)
     gStyle.SetLabelFont(62)
     gStyle.SetTitleFont(62)
+    
