4e6a10c2dc263fb494a7684552c11c97cd07ce9b
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-01-19 14:37:07 +0100

    Change label position and set default fill color to white

--- python/PlotStyle.py
+++ python/PlotStyle.py
@@ -4,7 +4,7 @@ colorRwthMagenta 	= TColor.GetColor("#E30066")
 colorRwthLightBlue 	= TColor.GetColor("#8EBAE5")
 colorRwthDarkBlue 	= TColor.GetColor("#00549F")
 
-labelCmsPrivateSimulation = TPaveText(0.1,0.901,0.4,0.93,"NDC")
+labelCmsPrivateSimulation = TPaveText(0.6,0.901,0.9,0.93,"NDC")
 labelCmsPrivateSimulation.AddText("#font[62]{CMS private}, #font[72]{simulation}")
 labelCmsPrivateSimulation.SetBorderSize(1)
 labelCmsPrivateSimulation.SetFillColor(0) # 0 == White
@@ -22,3 +22,4 @@ def setPlotStyle():
 	gStyle.SetTitleFont(62)
 	gStyle.SetLegendBorderSize(1)
 	gStyle.SetLegendFont(62)
+	gStyle.SetFillColor(0)
