376d96e045bbe27b45ba5fbd27c0e5d47452faac
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-02-09 18:12:01 +0100

    Add chimneys as graphs and converter funcitons from eta phi to hcal local coordiantes to plot style script. The file might need a renaming

--- python/PlotStyle.py
+++ python/PlotStyle.py
@@ -1,5 +1,5 @@
-from ROOT import gROOT,gStyle, TColor, TPaveText
-from math import sqrt
+from ROOT import gROOT,gStyle, TColor, TPaveText, TGraph, ROOT, Double, TBox
+from math import sqrt,pi
 
 colorRwthMagenta 	= TColor.GetColor("#E30066")
 colorRwthLightBlue 	= TColor.GetColor("#8EBAE5")
@@ -9,6 +9,56 @@ labelCmsPrivateSimulation = TPaveText(0.6,0.901,0.9,0.93,"NDC")
 labelCmsPrivateSimulation.AddText("#font[62]{CMS private}, #font[72]{simulation}")
 labelCmsPrivateSimulation.SetBorderSize(1)
 labelCmsPrivateSimulation.SetFillColor(0) # 0 == White
+
+chimney1 = TGraph()
+chimney1.SetPoint(0,.3425,1.48)
+chimney1.SetPoint(1,.3425,1.65)
+chimney1.SetPoint(2,.435,1.65)
+chimney1.SetPoint(3,.435,1.48)
+chimney1.SetPoint(4,.3425,1.48)
+chimney1.SetLineColor(ROOT.kRed)
+chimney1.SetLineWidth(2)
+
+chimney2 = TGraph()
+chimney2.SetPoint(0,-.3425,0.87)
+chimney2.SetPoint(1,-.3425,1.22)
+chimney2.SetPoint(2,-.435,1.22)
+chimney2.SetPoint(3,-.435,0.87)
+chimney2.SetPoint(4,-.3425,0.87)
+chimney2.SetLineColor(ROOT.kRed)
+chimney2.SetLineWidth(2)
+
+#Converts a graph in eta and phi to hcal coordinates with ieta and iphi
+def convertToHcalCoords(inputGraph):
+	gConvertedToiEtaiPhi = TGraph()
+	gConvertedToiEtaiPhi.SetMarkerStyle(6)
+	for i in range(0, inputGraph.GetN()):
+		x = Double(0)
+ 		y = Double(0)
+	 	inputGraph.GetPoint(i,x,y)
+	 	if( y < 0 ):
+			y += 2*pi
+		if( y > 2*pi ):
+			y -= 2*pi
+ 		xIEta = x/0.087 + ( 0.5 if x > 0 else -0.5 )
+  		yIPhi = y/0.087 + 1
+  	 	gConvertedToiEtaiPhi.SetPoint(i,xIEta,yIPhi)
+  	gConvertedToiEtaiPhi.GetXaxis().SetTitle("i#eta")
+	gConvertedToiEtaiPhi.GetYaxis().SetTitle("i#phi")
+  	return gConvertedToiEtaiPhi
+
+def drawHcalBoxesHcalCoords(canvas):
+    canvas.cd()
+    boxes = []
+    for i in range(-15,15):
+        for j in range(0,72):
+            box = TBox(i,j,(i+1),(j+1))
+            box.SetFillStyle(0)
+            box.SetLineColor(colorRwthMagenta)
+            box.SetLineWidth(2)
+            box.Draw()
+            boxes.append(box)
+    return boxes
 
 def setPlotStyle():
 	gStyle.SetPadGridX(1)
