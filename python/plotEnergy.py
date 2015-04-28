a25eb8d1f7b40fdcfdd33aa88dd362e56972ec94
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-04-28 10:21:20 +0200

    Add function for plotting energy normalized and not normalized

--- python/plotEnergy.py
+++ python/plotEnergy.py
@@ -7,6 +7,85 @@ import numpy as np
 DEBUG = 1
 prefix = '[plotEnergy] '
 
+def plotEnergyNormalized(folder, nameOfFile = 'L1MuonHistogram.root'):
+	if(folder == None):
+	    print 'Error! Filename as first argument needed.'
+	    return
+
+	if( not os.path.exists('plots')):
+	    os.mkdir('plots')
+	if( not os.path.exists('plots/' + folder)):
+		os.mkdir('plots/' + folder)
+
+	filename = folder + '/' + nameOfFile
+	if( not os.path.exists(filename)):
+		print 'Error! File ' + filename + ' does not exist!'
+		return
+	print 'Opening file:',filename
+	file = TFile.Open(filename)
+
+
+	#Set plot style
+	PlotStyle.setPlotStyle()
+
+	ho = file.Get("hoMuonAnalyzer/energy/horeco_Energy")
+	L1MuonAndHoMatch = file.Get('hoMuonAnalyzer/energy/L1MuonWithHoMatch_Energy')
+	L1MuonAndHoMatchAboveThr = file.Get('hoMuonAnalyzer/energy/L1MuonWithHoMatchAboveThr_Energy')
+	L1MuonAndHoMatchAboveThrFilt = file.Get('hoMuonAnalyzer/energy/L1MuonWithHoMatchAboveThrFilt_Energy')
+
+	canv = TCanvas("energieCanvas",'Energy canvas',1200,1200)
+	canv.SetLogy()
+
+	ho.SetStats(0)
+	ho.SetTitle('Normalized energy distribution of HO hits')
+	ho.GetXaxis().SetTitle('Reconstructed HO energy / GeV')
+	ho.GetYaxis().SetTitle('rel. fraction')
+	ho.GetXaxis().SetRangeUser(-2,6)
+
+	ho.SetLineColor(ROOT.kBlack)
+	ho.SetLineWidth(3)
+	ho.Scale(1/ho.Integral())
+	ho.Draw()
+	
+	label = PlotStyle.getLabelCmsPrivateSimulation()
+	label.Draw()
+	
+	legend = TLegend(0.5,0.65,0.9,0.9)
+	legend.AddEntry(ho,'All HO hits','l')
+	legend.Draw()
+
+	if(L1MuonAndHoMatch):
+		L1MuonAndHoMatch.SetLineColor(ROOT.kBlue)
+		L1MuonAndHoMatch.SetLineWidth(3)
+		L1MuonAndHoMatch.Scale(1/L1MuonAndHoMatch.Integral())
+		L1MuonAndHoMatch.Draw('same')
+		legend.AddEntry(L1MuonAndHoMatch,'L1Muon + HO match','l')
+		
+	if(L1MuonAndHoMatchAboveThr):
+		L1MuonAndHoMatchAboveThr.SetLineColor(ROOT.kRed)
+		L1MuonAndHoMatchAboveThr.SetLineWidth(3)
+		L1MuonAndHoMatchAboveThr.Scale(1/L1MuonAndHoMatchAboveThr.Integral())
+		L1MuonAndHoMatchAboveThr.Draw('same')
+		legend.AddEntry(L1MuonAndHoMatchAboveThr,'L1Muon + HO match > 0.2 GeV','l')
+
+
+	if(L1MuonAndHoMatchAboveThrFilt):
+		L1MuonAndHoMatchAboveThrFilt.SetLineColor(ROOT.kGreen)
+		L1MuonAndHoMatchAboveThrFilt.SetLineWidth(3)
+		L1MuonAndHoMatchAboveThrFilt.Scale(1/L1MuonAndHoMatchAboveThrFilt.Integral())
+		L1MuonAndHoMatchAboveThrFilt.Draw('same')
+		legend.AddEntry(L1MuonAndHoMatchAboveThrFilt,'L1Muon + HO match > 0.2 GeV (In Ho Geom.)','l')
+
+	
+
+	canv.SaveAs("plots/" + folder + "/energyNorm.png")
+	canv.SaveAs("plots/" + folder + "/energyNorm.pdf")
+
+	f = TFile.Open("plots/" + folder + "/energyNorm.root","RECREATE")
+	canv.Write()
+	f.Close()
+	return [canv,ho,L1MuonAndHoMatch, L1MuonAndHoMatchAboveThr,L1MuonAndHoMatchAboveThrFilt,label]
+
 def plotEnergy(folder, nameOfFile = 'L1MuonHistogram.root'):
 	if(folder == None):
 	    print 'Error! Filename as first argument needed.'
@@ -44,8 +123,10 @@ def plotEnergy(folder, nameOfFile = 'L1MuonHistogram.root'):
 
 	ho.SetLineColor(ROOT.kBlack)
 	ho.SetLineWidth(3)
-	ho.Scale(1/ho.Integral())
 	ho.Draw()
+	
+	label = PlotStyle.getLabelCmsPrivateSimulation()
+	label.Draw()
 	
 	legend = TLegend(0.5,0.65,0.9,0.9)
 	legend.AddEntry(ho,'All HO hits','l')
@@ -54,14 +135,12 @@ def plotEnergy(folder, nameOfFile = 'L1MuonHistogram.root'):
 	if(L1MuonAndHoMatch):
 		L1MuonAndHoMatch.SetLineColor(ROOT.kBlue)
 		L1MuonAndHoMatch.SetLineWidth(3)
-		L1MuonAndHoMatch.Scale(1/L1MuonAndHoMatch.Integral())
 		L1MuonAndHoMatch.Draw('same')
 		legend.AddEntry(L1MuonAndHoMatch,'L1Muon + HO match','l')
 		
 	if(L1MuonAndHoMatchAboveThr):
 		L1MuonAndHoMatchAboveThr.SetLineColor(ROOT.kRed)
 		L1MuonAndHoMatchAboveThr.SetLineWidth(3)
-		L1MuonAndHoMatchAboveThr.Scale(1/L1MuonAndHoMatchAboveThr.Integral())
 		L1MuonAndHoMatchAboveThr.Draw('same')
 		legend.AddEntry(L1MuonAndHoMatchAboveThr,'L1Muon + HO match > 0.2 GeV','l')
 
@@ -69,7 +148,6 @@ def plotEnergy(folder, nameOfFile = 'L1MuonHistogram.root'):
 	if(L1MuonAndHoMatchAboveThrFilt):
 		L1MuonAndHoMatchAboveThrFilt.SetLineColor(ROOT.kGreen)
 		L1MuonAndHoMatchAboveThrFilt.SetLineWidth(3)
-		L1MuonAndHoMatchAboveThrFilt.Scale(1/L1MuonAndHoMatchAboveThrFilt.Integral())
 		L1MuonAndHoMatchAboveThrFilt.Draw('same')
 		legend.AddEntry(L1MuonAndHoMatchAboveThrFilt,'L1Muon + HO match > 0.2 GeV (In Ho Geom.)','l')
 
@@ -81,7 +159,7 @@ def plotEnergy(folder, nameOfFile = 'L1MuonHistogram.root'):
 	f = TFile.Open("plots/" + folder + "/energy.root","RECREATE")
 	canv.Write()
 	f.Close()
-	return [canv,ho,L1MuonAndHoMatch, L1MuonAndHoMatchAboveThr,L1MuonAndHoMatchAboveThrFilt]
+	return [canv,ho,L1MuonAndHoMatch, L1MuonAndHoMatchAboveThr,L1MuonAndHoMatchAboveThrFilt,label]
 
 def plotEnergyVsEta(folder,sourceHistogram = 'L1MuonWithHoMatch_EnergyVsEta',filename = 'L1MuonHistogram.root'):
 	if(DEBUG):
