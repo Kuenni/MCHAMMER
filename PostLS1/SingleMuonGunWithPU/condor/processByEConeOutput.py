58405fda5d180d79e88a4f27e997ce18de96ac60
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-05-06 15:32:44 +0200

    Add two simple scripts to plot the results of the parameter scan

---
+++ PostLS1/SingleMuonGunWithPU/condor/processByEConeOutput.py
@@ -0,0 +1,27 @@
+import os,sys
+sys.path.append(os.path.abspath("../../../python"))
+
+from ROOT import TCanvas,TH2D
+
+from PlotStyle import setPlotStyle
+setPlotStyle()
+
+canvas = TCanvas("canvas","canvas",1200,1200)
+hist = getTH2D("hist","Parameter Scan, match by E_{Cone};#DeltaR;E_{Thr}",16,-0.0375,0.3625,16,-0.0375,0.3625)
+for filename in os.listdir('./results'):
+    if filename[-4:] == '.txt':
+        if filename.find('byECone') != -1:
+            file = open('./results/' + filename,'r')
+            file.readline()
+            line 	= file.readline().split()
+            deltaR 	= float(line[1])
+            eThr 	= float(line[2])
+            nCorrect = float(line[3])
+            nTotal 	= float(line[4])
+            hist.Fill(deltaR,eThr,nCorrect/nTotal*100)
+hist.SetStats(0)
+hist.Draw('colz')
+
+canvas.Update()
+
+raw_input('-->')
