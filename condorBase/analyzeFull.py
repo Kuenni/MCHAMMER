55d946148d26e191bb3e80f0ae08267870ec8ff8
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-05-21 15:15:49 +0200

    Add columns for total event numbers and efficiency calculation in the results file. Removed storing of root plots Restructured progress output

--- condorBase/analyzeFull.py
+++ condorBase/analyzeFull.py
@@ -24,7 +24,7 @@ gROOT.SetBatch()
 gROOT.ProcessLine("gErrorIgnoreLevel = 3000;")
 gROOT.ProcessLine(".L ./loader.C+");
 
-from matchingLibrary import calculateDeltaR, findBestHoMatchByEnergy, findBestHoMatchByDeltaR, findBestHoMatch
+from matchingLibrary import calculateDeltaR, findBestHoMatchByEnergy, findBestHoMatchByDeltaR, findBestHoMatch,findBestL1Match
 
 from PlotStyle import setPlotStyle,getTH1D
 setPlotStyle()
@@ -56,7 +56,7 @@ eThrList 	= np.arange(E_THR_START,E_THR_END,E_THR_STEPSIZE)
 
 #Prepare file for results
 resultFilename = 'results/analyzeFullResults_%s.txt' % (options.instance)
-resultHeader = "deltaR\tEThr\tnInside\tnTotal\n"
+resultHeader = "deltaR\tEThr\tnTimeInside\tnTimeTotal\tnMatchedTruth\tnTruthTotal\tnEvents\n"
 resultFile = open(resultFilename,'w')
 resultFile.write(resultHeader)
 
@@ -65,13 +65,7 @@ def analyze(deltaR = -1, eThr = -1):
 
 	deltaTimes = []
 	eventCounter = 0
-	
-	header = '| Delta R = %4.3f. E Thr = %4.3fGeV |' % (deltaR,eThr)
-	
-	print len(header)*'-'
-	print header
-	print len(header)*'-'
-	
+	print 'Delta time analysis:'
 	for event in file.dataTree:
 		#Tell us about the progress
 		eventCounter += 1
@@ -85,26 +79,51 @@ def analyze(deltaR = -1, eThr = -1):
 				deltaTimes.append(l1Object.bx*25. - hoMatch.time)
 	print
 	
-	histDeltaTime = getTH1D("histDeltaTimes","#DeltaTime;E_{Thr} / GeV;#DeltaTime / ns", 200,-100,100)
+# 	histDeltaTime = getTH1D("histDeltaTimes","#DeltaTime;#DeltaTime / ns;#", 200,-100,100)
 	
-	for deltaTime in deltaTimes:
-			histDeltaTime.Fill(deltaTime)
-	canvas = TCanvas('canvasDeltaTimesEThr','Delta Time vs. E_{Thr}',1200,1200)
-	histDeltaTime.SetStats(0)
-	histDeltaTime.Draw()
-	
-	canvas.Update()
+# 	for deltaTime in deltaTimes:
+# 			histDeltaTime.Fill(deltaTime)
+# 	canvas = TCanvas('canvasDeltaTimesEThr','Delta Time vs. E_{Thr}',1200,1200)
+# 	histDeltaTime.SetStats(0)
+# 	histDeltaTime.Draw()
+# 	
+# 	canvas.Update()
 	
 	print 'Done.'
 	
 	filenameTrunk = 'results/plots/analyzeFull-DeltaR%01d_%03d-EThr%01d_%03d_%s' % (int(deltaR),int(deltaR*1000),
 												int(eThr),int(eThr*1000),options.instance)
 	
-	canvas.SaveAs(filenameTrunk + '.png')
-	canvas.SaveAs(filenameTrunk + '.pdf')
-	canvas.SaveAs(filenameTrunk + '.root')
+# 	canvas.SaveAs(filenameTrunk + '.png')
+# 	canvas.SaveAs(filenameTrunk + '.pdf')
+# 	canvas.SaveAs(filenameTrunk + '.root')
 	
-	return deltaTimes,deltaR,eThr
+	return deltaTimes
+
+#Determine the number of successfully matched L1 and HO for the L1 associated to GEN
+def calculateEfficiency(deltaR = -1, eThr = -1):
+	nMatches = 0
+	nTotal = 0
+	eventCounter = 0
+	print 'Efficiency analysis:'
+	for event in file.dataTree:
+		#Tell us about the progress
+		eventCounter += 1
+		if(eventCounter%1000 == 0):
+			sys.stdout.write( '\rprocessing event %7d ==> %6.2f%% done.' % (eventCounter,eventCounter/float(tree.GetEntriesFast())*100))
+			sys.stdout.flush()
+		l1DataVector = event.l1MuonData
+		genDataVector = event.genMuonData
+		for genObject in genDataVector:
+			l1Match = findBestL1Match(genObject, l1DataVector,0.3)
+			if l1Match != None:
+				nTotal += 1
+				hoMatch = findBestHoMatch(l1Match,event.hoRecHitData,deltaR,eThr)
+				if(hoMatch != None):
+					nMatches += 1
+	print
+	print 'Done.'
+	return nMatches, nTotal,eventCounter
 
 #Run the full parameter loop
 progressCounter = 0
@@ -112,14 +131,19 @@ totalActionsCounter = len(deltaRList)*len(eThrList)
 for deltaR in deltaRList:
 	for eThr in eThrList:
 		progressCounter += 1
-		deltaTimes,deltaR,eThr = analyze(deltaR, eThr)
+		header = '| Delta R = %4.3f. E Thr = %4.3fGeV |' % (deltaR,eThr)
+		print len(header)*'-'
+		print header
+		print len(header)*'-'
+		deltaTimes = analyze(deltaR, eThr)
+		nMatchedTruth, nTotal,nEvents = calculateEfficiency(deltaR, eThr)
 		nInsideTimeWindow = 0
 		
 		for deltaTime in deltaTimes:
 			if abs(deltaTime) < 12.5:
 				nInsideTimeWindow += 1
 				
-		resultString = "%f\t%f\t%d\t%d\n" % (deltaR,eThr,nInsideTimeWindow,len(deltaTimes))
+		resultString = "%f\t%f\t%d\t%d\t%d\t%d\t%d\n" % (deltaR,eThr,nInsideTimeWindow,len(deltaTimes),nMatchedTruth,nTotal,nEvents)
 		resultFile.write(resultString)
 		
 		print 'Overall progress: %5.2f%%' % (progressCounter/float(totalActionsCounter)*100)
