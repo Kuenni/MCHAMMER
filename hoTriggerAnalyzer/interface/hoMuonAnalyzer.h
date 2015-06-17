6b3de7ea7541dc35671ba38d2c04d2e7e504fd54
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-06-17 16:45:06 +0200

    Fix indentation

--- hoTriggerAnalyzer/interface/hoMuonAnalyzer.h
+++ hoTriggerAnalyzer/interface/hoMuonAnalyzer.h
@@ -8,7 +8,7 @@
 
  Implementation:
      [Notes on implementation]
-*/
+ */
 //
 // Original Author:  Christopher Anelli  
 //         Created:  Fri, 16 May 2014 04:20:05 GMT
@@ -57,142 +57,142 @@ using namespace::std;
 
 class hoMuonAnalyzer : public edm::EDAnalyzer {
 public:
-  explicit hoMuonAnalyzer(const edm::ParameterSet&);
-  ~hoMuonAnalyzer();
-  
-  static void fillDescriptions(edm::ConfigurationDescriptions& 
-				   descriptions);
+	explicit hoMuonAnalyzer(const edm::ParameterSet&);
+	~hoMuonAnalyzer();
+
+	static void fillDescriptions(edm::ConfigurationDescriptions&
+			descriptions);
 
 private:
 
-  virtual void beginJob() override;
-  virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
-  virtual void endJob() override;
-  virtual void beginRun (const edm::Run& iRun, const edm::EventSetup& evSetup);
-  virtual void endRun(const edm::Run& iRun, const edm::EventSetup& evSetup);
-  
-  void defineTriggersOfInterest();
-  void printChannelQualities(const edm::EventSetup & iEvent);
-  void analyzeL1AndGenMatch(const edm::Event& iEvent,const edm::EventSetup& iSetup);
-  void analyzeNoSingleMuEventsL1Loop(const edm::Event& iEvent,const edm::EventSetup& iSetup);
-  void analyzeNoSingleMuEventsGenLoop(const edm::Event& iEvent,const edm::EventSetup& iSetup);
-  void analyzeEfficiencyWithGenLoop(const edm::Event& iEvent,const edm::EventSetup& iSetup);
-  void analyzeL1MuonsForGhosts(const edm::Event& iEvent,const edm::EventSetup& iSetup);
-  void fillEfficiencyHistograms(double ptMeasured,double ptReal,std::string key);
-  void fillHoGeomAcceptanceGraph(reco::GenParticle genParticle);
-  void analyzeHoDigiTiming(const edm::Event& iEvent);
+	virtual void beginJob() override;
+	virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+	virtual void endJob() override;
+	virtual void beginRun (const edm::Run& iRun, const edm::EventSetup& evSetup);
+	virtual void endRun(const edm::Run& iRun, const edm::EventSetup& evSetup);
 
-  const reco::GenParticle* getBestGenMatch(float,float);
-  const l1extra::L1MuonParticle* getBestL1MuonMatch(double eta, double phi);
-  const l1extra::L1MuonParticle* getMatchedL1Object(trigger::TriggerObject,edm::Handle<l1extra::L1MuonParticleCollection>);
+	void defineTriggersOfInterest();
+	void printChannelQualities(const edm::EventSetup & iEvent);
+	void analyzeL1AndGenMatch(const edm::Event& iEvent,const edm::EventSetup& iSetup);
+	void analyzeNoSingleMuEventsL1Loop(const edm::Event& iEvent,const edm::EventSetup& iSetup);
+	void analyzeNoSingleMuEventsGenLoop(const edm::Event& iEvent,const edm::EventSetup& iSetup);
+	void analyzeEfficiencyWithGenLoop(const edm::Event& iEvent,const edm::EventSetup& iSetup);
+	void analyzeL1MuonsForGhosts(const edm::Event& iEvent,const edm::EventSetup& iSetup);
+	void fillEfficiencyHistograms(double ptMeasured,double ptReal,std::string key);
+	void fillHoGeomAcceptanceGraph(reco::GenParticle genParticle);
+	void analyzeHoDigiTiming(const edm::Event& iEvent);
 
-  bool hasL1Match(trigger::TriggerObject,edm::Handle<l1extra::L1MuonParticleCollection>);
-  bool hasHoHitInGrid(GlobalPoint direction,int gridSize);
-  bool processTriggerDecision(string algorithmName,const edm::Event& );
+	const reco::GenParticle* getBestGenMatch(float,float);
+	const l1extra::L1MuonParticle* getBestL1MuonMatch(double eta, double phi);
+	const l1extra::L1MuonParticle* getMatchedL1Object(trigger::TriggerObject,edm::Handle<l1extra::L1MuonParticleCollection>);
 
-  int findMaximumTimeSlice(const HODataFrame* dataFrame);
-  double calculateHitTimeFromDigi(const HODataFrame* dataFrame);
-  bool isFrameAboveThr(const HODataFrame* dataFrame);
-  int get4TsAdcSum(const HODataFrame* dataFrame, int sliceMax);
-  float timeshift_ns_hbheho(float wpksamp);
+	bool hasL1Match(trigger::TriggerObject,edm::Handle<l1extra::L1MuonParticleCollection>);
+	bool hasHoHitInGrid(GlobalPoint direction,int gridSize);
+	bool processTriggerDecision(string algorithmName,const edm::Event& );
 
-  const HORecHit* findHoRecHitById(DetId id);
-  const HODataFrame* findHoDigiById(DetId id);
+	int findMaximumTimeSlice(const HODataFrame* dataFrame);
+	double calculateHitTimeFromDigi(const HODataFrame* dataFrame);
+	bool isFrameAboveThr(const HODataFrame* dataFrame);
+	int get4TsAdcSum(const HODataFrame* dataFrame, int sliceMax);
+	float timeshift_ns_hbheho(float wpksamp);
 
-  TrackDetMatchInfo* getTrackDetMatchInfo(reco::GenParticle,const edm::Event& iEvent,const edm::EventSetup& iSetup);
+	const HORecHit* findHoRecHitById(DetId id);
+	const HODataFrame* findHoDigiById(DetId id);
 
-  edm::Service<TFileService> _fileService;
+	TrackDetMatchInfo* getTrackDetMatchInfo(reco::GenParticle,const edm::Event& iEvent,const edm::EventSetup& iSetup);
 
-  edm::InputTag _genInput;
-  edm::InputTag _l1MuonInput;
-  edm::InputTag _horecoInput;
-  edm::InputTag _l1MuonGenMatchInput;
-  edm::InputTag _hltSumAODInput;
+	edm::Service<TFileService> _fileService;
 
-  edm::Handle<reco::GenParticleCollection> truthParticles;
-  edm::Handle<l1extra::L1MuonParticleCollection> l1Muons;
-  edm::Handle<HORecHitCollection> hoRecoHits;
-  edm::Handle<HODigiCollection> hoDigis;
-  edm::Handle<reco::GenParticleMatch> l1MuonGenMatches;
-  edm::Handle<edm::View<l1extra::L1MuonParticle> > l1MuonView;
-  edm::Handle<reco::MuonCollection> recoMuons;
+	edm::InputTag _genInput;
+	edm::InputTag _l1MuonInput;
+	edm::InputTag _horecoInput;
+	edm::InputTag _l1MuonGenMatchInput;
+	edm::InputTag _hltSumAODInput;
 
-  edm::ESHandle<CaloGeometry> caloGeo;
-  edm::ESHandle<DetIdAssociator> hoDetIdAssociator_;
-  edm::ESHandle<MagneticField> theMagField;
+	edm::Handle<reco::GenParticleCollection> truthParticles;
+	edm::Handle<l1extra::L1MuonParticleCollection> l1Muons;
+	edm::Handle<HORecHitCollection> hoRecoHits;
+	edm::Handle<HODigiCollection> hoDigis;
+	edm::Handle<reco::GenParticleMatch> l1MuonGenMatches;
+	edm::Handle<edm::View<l1extra::L1MuonParticle> > l1MuonView;
+	edm::Handle<reco::MuonCollection> recoMuons;
 
-  HistogramBuilder histogramBuilder;
+	edm::ESHandle<CaloGeometry> caloGeo;
+	edm::ESHandle<DetIdAssociator> hoDetIdAssociator_;
+	edm::ESHandle<MagneticField> theMagField;
 
-  TrackDetectorAssociator assoc;
-  TrackAssociatorParameters assocParams;
+	HistogramBuilder histogramBuilder;
 
-  HoMatcher* hoMatcher;
+	TrackDetectorAssociator assoc;
+	TrackAssociatorParameters assocParams;
 
-  /*
-   * Maps of selected hlt triggers to get the trigger decisions,
-   * and hlt filters to get the trigger objects.
-   */
+	HoMatcher* hoMatcher;
 
-  std::map<std::string, std::string> hltNamesOfInterest;
-  std::map<std::string, edm::InputTag> hltFiltersOfInterest;
-  std::vector<trigger::TriggerObject> hltTriggerObjects;
-  std::map<std::string, std::vector<trigger::TriggerObject> > hltTriggerObjectsOfInterest;
-  
-  // I would prefer to run without an InputTag, the L1GtUtility should be     
-  // able to find it automatically from the Providence information.        
+	/*
+	 * Maps of selected hlt triggers to get the trigger decisions,
+	 * and hlt filters to get the trigger objects.
+	 */
 
-  //edm::InputTag m_l1GtTmLInputTag;
-  
-  L1GtUtils m_l1GtUtils;
-  string singleMu3TrigName;
-  string doubleMu0TrigName;
-  string doubleMu5TrigName;
-  bool trigDecision;
-  bool singleMu3Trig,doubleMu0Trig;
-  bool debug;
-  bool firstRun;
-  /**
-   * Prepare a TTree and some vectors for storing the data.
-   * Analysis and adding of new plots should be faster, and cmssw would only needed
-   * to be rerun if a new data member is needed. The data in the vectors is stored in structs
-   * for the different objects (L1, HoRechit, Gen)
-   */
+	std::map<std::string, std::string> hltNamesOfInterest;
+	std::map<std::string, edm::InputTag> hltFiltersOfInterest;
+	std::vector<trigger::TriggerObject> hltTriggerObjects;
+	std::map<std::string, std::vector<trigger::TriggerObject> > hltTriggerObjectsOfInterest;
 
-  TTree* dataTree;
+	// I would prefer to run without an InputTag, the L1GtUtility should be
+	// able to find it automatically from the Providence information.
 
-  std::vector<L1MuonData>* l1MuonVector;
-  std::vector<HoRecHitData>* hoRecHitVector;
-  std::vector<GenMuonData>* genMuonVector;
+	//edm::InputTag m_l1GtTmLInputTag;
 
-  /**
-   * Energy threshold for HO rec hits
-   */
-  float threshold;
+	L1GtUtils m_l1GtUtils;
+	string singleMu3TrigName;
+	string doubleMu0TrigName;
+	string doubleMu5TrigName;
+	bool trigDecision;
+	bool singleMu3Trig,doubleMu0Trig;
+	bool debug;
+	bool firstRun;
+	/**
+	 * Prepare a TTree and some vectors for storing the data.
+	 * Analysis and adding of new plots should be faster, and cmssw would only needed
+	 * to be rerun if a new data member is needed. The data in the vectors is stored in structs
+	 * for the different objects (L1, HoRechit, Gen)
+	 */
 
-  /**
-   * ADC Threshold for 4 TS HO Digi
-   */
-  int ADC_THR;
+	TTree* dataTree;
 
-  /**
-   * Maximum delta R to be used for matching
-   */
-  float deltaR_Max;
+	std::vector<L1MuonData>* l1MuonVector;
+	std::vector<HoRecHitData>* hoRecHitVector;
+	std::vector<GenMuonData>* genMuonVector;
 
-  /**
-   * max delta R for matching a L1Muon object to a gen particle
-   */
-  float deltaR_L1MuonMatching;
+	/**
+	 * Energy threshold for HO rec hits
+	 */
+	float threshold;
 
-  //toFigureOutL1VariableBinning
-  std::list<float> listL1MuonPt;
+	/**
+	 * ADC Threshold for 4 TS HO Digi
+	 */
+	int ADC_THR;
 
-  std::string coutPrefix;
+	/**
+	 * Maximum delta R to be used for matching
+	 */
+	float deltaR_Max;
 
-  //virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
-  //virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+	/**
+	 * max delta R for matching a L1Muon object to a gen particle
+	 */
+	float deltaR_L1MuonMatching;
 
-      // ----------member data ---------------------------
+	//toFigureOutL1VariableBinning
+	std::list<float> listL1MuonPt;
+
+	std::string coutPrefix;
+
+	//virtual void beginLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+	//virtual void endLuminosityBlock(edm::LuminosityBlock const&, edm::EventSetup const&) override;
+
+	// ----------member data ---------------------------
 };
 
 #endif
