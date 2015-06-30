b0e2c68d0b797bf827f18d20fe59304533341e98
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-06-30 14:54:36 +0200

    Bugfix: Correct number of events to process in the template

--- python/runConfig_template.py
+++ python/runConfig_template.py
@@ -9,7 +9,7 @@ process.TFileService = cms.Service("TFileService",
                                    	fileName=cms.string('L1MuonHistogramPooja%INSTANCE%.root'),
                                    )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
 
 
 import FWCore.Utilities.FileUtils as FileUtils
