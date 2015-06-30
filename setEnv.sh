116b363d954a9d7587b80759dac6cc232bfdfb50
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-06-30 13:41:36 +0200

    Setup environment variables for using the source python directory. This way, copying of scripts is not necessary any more

---
+++ setEnv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Add the python directory to the path and the Pythonpath
+export PATH=$CMSSW_BASE/src/HoMuonTrigger/python:$PATH
+export PYTHONPATH=$CMSSW_BASE/src/HoMuonTrigger/python:$PYTHONPATH
+
