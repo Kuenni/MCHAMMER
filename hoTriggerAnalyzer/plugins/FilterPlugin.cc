4e3bd5097b519af804f288cc15a76f7a3cc167f7
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-07-21 16:12:24 +0200

    BugFix: Swapped calculation of delta Eta and delta Phi

--- hoTriggerAnalyzer/plugins/FilterPlugin.cc
+++ hoTriggerAnalyzer/plugins/FilterPlugin.cc
@@ -23,7 +23,7 @@ HORecHitCollection FilterPlugin::cleanHoRecHits(HORecHitCollection hoRecHits,dou
 }
 
 double FilterPlugin::wrapCheck(double phi1, double phi2){
-  float delta_phi = phi1 - phi2;
+  float delta_phi = phi2 - phi1;
   if(delta_phi < -M_PI){
     return (2*M_PI + delta_phi);
   }
@@ -37,7 +37,7 @@ bool FilterPlugin::isInsideDeltaR(double eta1, double eta2, double phi1, double 
 
 	float delta_eta, delta_phi;
 
-	delta_eta = eta1 - eta2;
+	delta_eta = eta2 - eta1;
 	delta_phi = wrapCheck(phi1,phi2); //Finds difference in phi
 
 	if(pow(delta_eta,2)+pow(delta_phi,2) <= pow(deltaR_Max,2)) return true;
