6fe1b1312d374745958e7175a43ff0daee3355c7
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-08-11 15:41:24 +0200

    Do some renaming and add a parameter for the get graph function to control how many files have to be processed. Avoids running out of memory for graphs with large data point samples

--- python/plotting/RootFileHandler.py
+++ python/plotting/RootFileHandler.py
@@ -41,13 +41,12 @@ class RootFileHandler:
 	and then, the histograms from the other files are added in a loop
 	'''
 	def getHistogram(self,histoname):
-		histNew = None
-		file = TFile(self.fileNameList[0],'READ')
-		histNew = file.Get(histoname).Clone()
+		rootfile = TFile(self.fileNameList[0],'READ')
+		histNew = rootfile.Get(histoname).Clone()
 		histNew.SetDirectory(0)
 		for i in range(1,len(self.fileNameList)):
-			file = TFile(self.fileNameList[i],'READ')
-			histNew.Add(file.Get(histoname))
+			rootfile = TFile(self.fileNameList[i],'READ')
+			histNew.Add(rootfile.Get(histoname))
 		return histNew
 	
 	'''
@@ -55,27 +54,31 @@ class RootFileHandler:
 	The points from the additional files are added to the graph
 	from the first file
 	'''
-	def getGraph(self,graphname):
-		newGraph = None
-		file = TFile(self.fileNameList[0],'READ')
-		graph = file.Get(graphname)
+	def getGraph(self,graphname,filesToProcess = -1):
+		rootfile = TFile(self.fileNameList[0],'READ')
+		graph = rootfile.Get(graphname)
 		nTotal = 0
 		counter = 0
 		
-		for i in range(0,len(self.fileNameList)):
-			file = TFile(self.fileNameList[i],'READ')
-			g = file.Get(graphname)
+		#Check whether a number of result files to process is given
+		fileRange = filesToProcess if (filesToProcess != -1) else len(self.fileNameList)
+		#Then check that we don't try process more files than available
+		fileRange = min(len(self.fileNameList), fileRange )
+		
+		for i in range(0,fileRange):
+			rootfile = TFile(self.fileNameList[i],'READ')
+			g = rootfile.Get(graphname)
 			nTotal += g.GetN()
 		commandLine.output('getGraph(%s) found %d points to process' % (graphname,nTotal))
-		for i in range(1,len(self.fileNameList)):
-			file = TFile(self.fileNameList[i],'READ')
-			g2 = file.Get(graphname)
+		for i in range(1,fileRange):
+			rootfile = TFile(self.fileNameList[i],'READ')
+			g2 = rootfile.Get(graphname)
 			x = Double(0)
- 			y = Double(0)
- 			for j in range(0,g2.GetN()):
- 				counter += 1
- 				if (counter % 100000 == 0):
- 					commandLine.printProgress(counter,nTotal)
+			y = Double(0)
+			for j in range(0,g2.GetN()):
+				counter += 1
+				if (counter % 100000 == 0):
+					commandLine.printProgress(counter,nTotal)
 				g2.GetPoint(j,x,y)
 				graph.SetPoint(graph.GetN(),x,y)
 		return graph
