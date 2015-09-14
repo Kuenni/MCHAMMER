66a54fbd8b9982bfba2d8d410f9b7d0848d85440
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-09-14 14:22:19 +0200

    Fix double plotting of same function. Instead plot the function that was not used before

--- python/makePhishiftPlots.py
+++ python/makePhishiftPlots.py
@@ -6,7 +6,7 @@ from phishift.DeltaPhi import *
 res4 = plotL1PhiVsHoPhi()
 res5 = plotL1PhiVsHoIPhi()
 res = plotDeltaPhiVsL1Phi()
-res2 = plotDeltaPhiVsL1Phi()
+res2 = plotDeltaPhiVsL1Pt()
 res3 = plotDeltaPhiVsGenPt()
 
 raw_input('-->')
