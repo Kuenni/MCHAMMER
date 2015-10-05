d6ae48a18660112ecfc0491529f9f8b4d6bd134c
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-10-05 14:16:59 +0200

    Add CLI colors to output module

--- python/plotting/OutputModule.py
+++ python/plotting/OutputModule.py
@@ -21,3 +21,13 @@ class CommandLineHandler:
 
 	def __init__(self,outputPrefix):
 		self.prefix = outputPrefix
+		
+class CliColors:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
