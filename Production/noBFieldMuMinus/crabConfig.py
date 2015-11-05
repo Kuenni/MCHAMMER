6f3e7b493197731b55c51943fce993e7c76620b3
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-11-05 19:29:59 +0100

    Add production configuration for no Bfield single mu minus

---
+++ Production/noBFieldMuMinus/crabConfig.py
@@ -0,0 +1,21 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'noBMuMinus'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.User.voGroup = 'dcms'
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'SingleMuPt5to200_cfi_GEN_SIM_DIGI_DIGI2RAW_RAW2DIGI_L1_L1Reco_Reco.py'
+config.JobType.maxMemoryMB = 2500
+
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10000
+NJOBS = 100  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/user/%s/SingleMuMinus' % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Site.storageSite = 'T2_DE_RWTH'
