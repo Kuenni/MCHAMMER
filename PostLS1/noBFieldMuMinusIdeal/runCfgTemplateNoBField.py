89aacd4419776cfa2ceab0697952f4abff58a69c
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2015-11-07 19:39:41 +0100

    Adjust the GT also in the other configs

--- PostLS1/noBFieldMuMinusIdeal/runCfgTemplateNoBField.py
+++ PostLS1/noBFieldMuMinusIdeal/runCfgTemplateNoBField.py
@@ -6,14 +6,14 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.TFileService = cms.Service("TFileService",
-                                   	fileName=cms.string('L1MuonHistogramsNoBFieldMuMinusIdeal.root'),
+                                   	fileName=cms.string('L1MuonHistogramPooja%INSTANCE%.root'),
                                    )
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1))
 
 
 import FWCore.Utilities.FileUtils as FileUtils
-mylist = FileUtils.loadListFromFile('cmsswSourceFiles')
+mylist = FileUtils.loadListFromFile('sourceList%INSTANCE%')
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
@@ -27,6 +27,7 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
 process.load('SimGeneral.MixingModule.mixNoPU_cfi')
 process.load('Configuration.Geometry.GeometryExtended2015Reco_cff')
+process.load('Configuration.Geometry.GeometryExtended2015_cff')
 process.load('Configuration.StandardSequences.MagneticField_0T_cff')
 process.load('Configuration.StandardSequences.Generator_cff')
 process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
@@ -34,14 +35,6 @@ process.load('GeneratorInterface.Core.genFilterSummary_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.L1Reco_cff')
 
-#From github g. petrucciani code
-#process.load("PhysicsTools.PatAlgos.patSequences_cff")
-#process.load("MuonAnalysis.MuonAssociators.muonL1Match_cfi")
-#process.muonL1Match.preselection = cms.string("")
-#process.allLayer1Muons.trigPrimMatch = cms.VInputTag(
-#    cms.InputTag("muonL1Match"),
-#    cms.InputTag("muonL1Match","propagatedReco"),
-#)
 
 from TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi import *
 #process.load('TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi')
@@ -58,7 +51,7 @@ process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 from Configuration.AlCa.GlobalTag import GlobalTag
-process.GlobalTag.globaltag = "DESIGN72_V2::All"
+process.GlobalTag.globaltag = %GLOBALTAG%
 print process.GlobalTag.globaltag
 
 parameters = TrackAssociatorParameterBlock.TrackAssociatorParameters
@@ -110,26 +103,6 @@ process.options = cms.untracked.PSet(
 
 )
 
-process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
-    splitLevel = cms.untracked.int32(0),
-    eventAutoFlushCompressedSize = cms.untracked.int32(1048576),
-    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
-    fileName = cms.untracked.string('SingleMuWithHoReco.root'),
-    dataset = cms.untracked.PSet(
-        filterName = cms.untracked.string(''),
-       dataTier = cms.untracked.string('')
-    )
-#    ,SelectEvents = cms.untracked.PSet(
-#        SelectEvents = cms.vstring('l1extra_step')
-#    )
-)
-
-process.FEVTDEBUGHLToutput.outputCommands.append('keep *_*CaloTower*_*_*')
-process.FEVTDEBUGHLToutput.outputCommands.append('keep *_*caloTower*_*_*')
-process.FEVTDEBUGHLToutput.outputCommands.append('keep *_*calotower*_*_*')
-
-process.end = cms.EndPath(process.FEVTDEBUGHLToutput)
-
 process.genfilter = cms.EDFilter("MCSingleParticleFilter",
 #	Status = cms.untracked.vint32(1,1),
 	MinPt = cms.untracked.vdouble(5.0,5.0),
@@ -137,75 +110,6 @@ process.genfilter = cms.EDFilter("MCSingleParticleFilter",
 	MaxEta = cms.untracked.vdouble(0.8,0.8),
 	ParticleID = cms.untracked.vint32(13,-13),
 	 )
-
-process.myCaloTowerMaker = cms.EDProducer( "CaloTowersCreator",
-    EBSumThreshold = cms.double( 0.2 ),
-    MomHBDepth = cms.double( 0.2 ),
-    UseEtEBTreshold = cms.bool( False ),
-    hfInput = cms.InputTag( "hltHfreco" ),
-    AllowMissingInputs = cms.bool( True ),
-    MomEEDepth = cms.double( 0.0 ),
-    EESumThreshold = cms.double( 0.45 ),
-    HBGrid = cms.vdouble(  ),
-    HcalAcceptSeverityLevelForRejectedHit = cms.uint32( 9999 ),
-    HBThreshold = cms.double( 0.7 ),
-    EcalSeveritiesToBeUsedInBadTowers = cms.vstring(  ),
-    UseEcalRecoveredHits = cms.bool( False ),
-    MomConstrMethod = cms.int32( 1 ),
-    MomHEDepth = cms.double( 0.4 ),
-    HcalThreshold = cms.double( -1000.0 ),
-    HF2Weights = cms.vdouble(  ),
-    HOWeights = cms.vdouble(  ),
-    EEGrid = cms.vdouble(  ),
-    UseSymEBTreshold = cms.bool( False ),
-    EEWeights = cms.vdouble(  ),
-    EEWeight = cms.double( 1.0 ),
-    UseHO = cms.bool( True ),
-    HBWeights = cms.vdouble(  ),
-    HF1Weight = cms.double( 1.0 ),
-    HF2Grid = cms.vdouble(  ),
-    HEDWeights = cms.vdouble(  ),
-    HEDGrid = cms.vdouble(  ),
-    EBWeight = cms.double( 1.0 ),
-    HF1Grid = cms.vdouble(  ),
-    EBWeights = cms.vdouble(  ),
-    HOWeight = cms.double( 1.0 ),
-    HESWeight = cms.double( 1.0 ),
-    HESThreshold = cms.double( 0.8 ),
-    hbheInput = cms.InputTag( "hltHbhereco" ),
-    HF2Weight = cms.double( 1.0 ),
-    HF2Threshold = cms.double( 0.85 ),
-    HcalAcceptSeverityLevel = cms.uint32( 9 ),
-    EEThreshold = cms.double( 0.3 ),
-    HOThresholdPlus1 = cms.double( 0 ),
-    HOThresholdPlus2 = cms.double( 0 ),
-    HF1Weights = cms.vdouble(  ),
-    hoInput = cms.InputTag( "horeco" ),
-    HF1Threshold = cms.double( 0.5 ),
-    HOThresholdMinus1 = cms.double( 0 ),
-    HESGrid = cms.vdouble(  ),
-    EcutTower = cms.double( -1000.0 ),
-    UseRejectedRecoveredEcalHits = cms.bool( False ),
-    UseEtEETreshold = cms.bool( False ),
-    HESWeights = cms.vdouble(  ),
-    EcalRecHitSeveritiesToBeExcluded = cms.vstring( 'kTime',
-      'kWeird',
-      'kBad' ),
-    HEDWeight = cms.double( 1.0 ),
-    UseSymEETreshold = cms.bool( False ),
-    HEDThreshold = cms.double( 0.8 ),
-    EBThreshold = cms.double( 0.07 ),
-    UseRejectedHitsOnly = cms.bool( False ),
-    UseHcalRecoveredHits = cms.bool( False ),
-    HOThresholdMinus2 = cms.double( 0 ),
-    HOThreshold0 = cms.double( 0 ),
-    ecalInputs = cms.VInputTag( 'hltEcalRecHitAll:EcalRecHitsEB','hltEcalRecHitAll:EcalRecHitsEE' ),
-    UseRejectedRecoveredHcalHits = cms.bool( False ),
-    MomEBDepth = cms.double( 0.3 ),
-    HBWeight = cms.double( 1.0 ),
-    HOGrid = cms.vdouble(  ),
-    EBGrid = cms.vdouble(  )
-)
 
 
 #Try using different source for hoReco
@@ -217,20 +121,17 @@ process.horeco_step = cms.Path(process.horeco)
 process.l1MuonGenMatch_step = cms.Path(process.l1MuonGenMatch)
 process.demo_step = cms.Path(process.hoMuonAnalyzer)
 process.L1Reco_step = cms.Path(process.L1Reco)
-#process.muonL1Match_step = cms.Path(process.muonL1Match)
 
 process.p = cms.Path(process.genfilter*
 					#*process.L1Reco*
 					process.l1MuonGenMatch*
 					process.horeco*
-					process.myCaloTowerMaker*
-					#process.muonL1Match*
 					process.hoMuonAnalyzer*
 					process.hoDigiAnalyzer)
 
 #Schedule Definition
 process.schedule = cms.Schedule(
-	process.p,process.end
+	process.p
 	)
 
 # Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.postLS1Customs
