e472dd5b4e1a8273af7ae0b8c5093afdeacd4ba2
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2016-01-05 14:29:43 +0100

    Finalize the plotting module for Ho Rec Hit threshold scans

--- python/efficiency/HoThresholdScan.py
+++ python/efficiency/HoThresholdScan.py
@@ -1,8 +1,9 @@
 from plotting.Plot import Plot
 
 from ROOT import TCanvas
-from plotting.Utils import getTGraphErrors
+from plotting.Utils import getTGraphErrors, getLegend
 import math
+from plotting.PlotStyle import colorRwthDarkBlue, setupAxes, colorRwthMagenta
 
 class HoThresholdScan(Plot):
 	
@@ -12,9 +13,14 @@ class HoThresholdScan(Plot):
 		
 	def plotHoThresholdScan(self):
 		canvas = TCanvas('cThresholdScan')
+		canvas.SetLogy()
 		xVals = []
 		yVals = []
 		yErr = []
+		
+		meanValues = []
+		rmsValues = []
+		
 		for i in range(0,199):
 			hist = self.fileHandler.getHistogram('hoMuonAnalyzer/multiplicity/recHitThrScan%d_Multiplicity' % i)
 			hist.GetXaxis().SetRangeUser(0,50)
@@ -23,6 +29,31 @@ class HoThresholdScan(Plot):
 			yVals.append(hist.GetBinCenter(hist.GetMaximumBin()))
 			yErr.append(math.sqrt(yVals[-1]))
 			xVals.append((i+1)*0.025)
- 		graph = getTGraphErrors(xVals, yVals, ey=yErr)
- 		graph.Draw('ap')
- 		return canvas, graph
+			meanValues.append(hist.GetMean())
+			rmsValues.append(hist.GetRMS())
+		graph = getTGraphErrors(xVals, yVals, ey=yErr)
+		graph.SetMarkerStyle(20)
+		graph.SetMarkerColor(colorRwthDarkBlue)
+		graph.SetLineColor(colorRwthDarkBlue)
+		graph.SetTitle('Number of HORecHits per Event;E_{Thr} / GeV;# per Event')
+		setupAxes(graph)
+		graph.GetXaxis().SetRangeUser(0,2)
+		graph.Draw('ap')
+		
+		graphMean = getTGraphErrors(xVals, meanValues, ey=rmsValues)
+		graphMean.SetMarkerStyle(21)
+		graphMean.SetMarkerColor(colorRwthMagenta)
+		graphMean.SetLineColor(colorRwthMagenta)
+		graphMean.Draw('samep')
+		
+		legend = getLegend(y2 = 0.9)
+		legend.AddEntry(graph,'Most frequent # per Evt.','ep')
+		legend.AddEntry(graphMean,'Mean # per Evt.','ep')
+		legend.Draw()
+		
+		label = self.drawLabel()
+		
+		canvas.Update()
+		canvas.SaveAs('plots/hoThresholdScan/hoThresholdScan.gif')
+		canvas.SaveAs('plots/hoThresholdScan/hoThresholdScan.pdf')
+		return canvas, graph,legend,graphMean,label
