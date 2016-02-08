5b2e1c8f43c9a7af72808f4f2c635403962eee52
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2016-02-08 13:03:53 +0100

    Now really undo the formatting

--- hoTriggerAnalyzer/interface/hoMuonAnalyzer.h
+++ hoTriggerAnalyzer/interface/hoMuonAnalyzer.h
@@ -101,9 +101,9 @@ private:
 
 	void recoControlPlots();
 
-	const l1extra::L1MuonParticle* getMatchedL1Object(trigger::TriggerObject,edm::Handle<l1extra::L1MuonParticleCollection>);
-	const reco::GenParticle* getBestGenMatch(float eta,float phi);
+	const reco::GenParticle* getBestGenMatch(float,float);
 	const reco::Muon* getBestRecoMatch(float eta, float phi);
+	const l1extra::L1MuonParticle* getMatchedL1Object(trigger::TriggerObject,edm::Handle<l1extra::L1MuonParticleCollection>);
 	const reco::Vertex getPrimaryVertex();
 
 	bool processTriggerDecision(string algorithmName,const edm::Event& );
@@ -121,15 +121,15 @@ private:
 	edm::InputTag _l1MuonGenMatchInput;
 	edm::InputTag _hltSumAODInput;
 
-	edm::Handle<edm::View<l1extra::L1MuonParticle> > l1MuonView;
-	edm::Handle<HORecHitCollection> hoRecoHits;
-	edm::Handle<l1extra::L1MuonParticleCollection> l1Muons;
 	edm::Handle<reco::BeamSpot> recoBeamSpotHandle;
 	edm::Handle<reco::GenParticleCollection> truthParticles;
+	edm::Handle<l1extra::L1MuonParticleCollection> l1Muons;
+	edm::Handle<HORecHitCollection> hoRecoHits;
 	edm::Handle<reco::GenParticleMatch> l1MuonGenMatches;
+	edm::Handle<edm::View<l1extra::L1MuonParticle> > l1MuonView;
 	edm::Handle<reco::MuonCollection> recoMuons;
-	edm::Handle<reco::VertexCollection> vertexColl;
 	edm::Handle<std::vector<pat::Muon>> patMuons;
+	edm::Handle<reco::VertexCollection> vertexColl;
 
 	edm::ESHandle<CaloGeometry> caloGeo;
 	edm::ESHandle<MagneticField> theMagField;
