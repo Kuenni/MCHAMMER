1261f3a1c4334408442adf2eaca334176d15f21a
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2016-02-16 10:56:40 +0100

    Work on the cutflow histogram

--- python/efficiency/Counters.py
+++ python/efficiency/Counters.py
@@ -1,12 +1,11 @@
 from plotting.Plot import Plot
 
-from ROOT import TCanvas,TH1D,TH2D
+from ROOT import TCanvas,TH1D,TH2D,TText
 from plotting.PlotStyle import calcSigma, colorRwthDarkBlue, colorRwthMagenta,\
-	colorRwthTuerkis, setupAxes, setupPalette
-from plotting.Utils import getLegend, L1_ETA_BIN, L1_PHI_BIN, fillGraphIn2DHist,calcPercent
+	colorRwthTuerkis, setupAxes, setupPalette, colorRwthOrange
+from plotting.Utils import getLegend, L1_ETA_BIN, L1_PHI_BIN, fillGraphIn2DHist,calcPercent,getXinNDC
 
 import math
-from termios import NL1
 
 class Counters(Plot):
 
@@ -41,70 +40,89 @@ class Counters(Plot):
 		
 		self.commandLine.output('###############################################')
 		for i,h in enumerate(histogramList):
-			self.commandLine.output('%s:\t\t%d\t=> %5.2f' % (names[i],h.GetBinContent(2),calcPercent(h.GetBinContent(2),nL1)))
+			self.commandLine.output('%-20s:%8d\t=> %5.2f' % (names[i],h.GetBinContent(2),calcPercent(h.GetBinContent(2),nL1)))
 		self.commandLine.output('###############################################')
 
 		
-		nEvents 	= hEvent.GetBinContent(2)
-		nAll 		= hAllL1.GetBinContent(2)
-		nAll3x3 	= hAllL13x3.GetBinContent(2)
-		nTight 		= hTightL1.GetBinContent(2)
-		nTight3x3	= hTightL13x3.GetBinContent(2)
+		nL1Reco 		= hL1Reco.GetBinContent(2)
+		nL1RecoHo 		= hL1RecoHo.GetBinContent(2)
+		nL1RecoHoTight 	= hL1RecoHoTight.GetBinContent(2)
+		nL1RecoTight 	= hL1RecoTight.GetBinContent(2)
+		nL1RecoTightHo	= hL1RecoTightHo.GetBinContent(2)
 		
 		N_BINS = 4
 		
-		binContents = [nEvents,nAll,nTight,nTight3x3]
-		binLabels = ['Events','L1','L1 Tight','L1 Tight 3x3']
+		binContents = [nL1,nL1Reco,nL1RecoHo,nL1RecoHoTight]
+		binLabels = ['L1','L1 -> Reco','L1 + R -> HO','L1 + R + HO -> tight']
 		
+		binContentsInverted = [nL1,nL1Reco,nL1RecoTight,nL1RecoTightHo]
+		binLabelsInverted = ['L1','L1 -> Reco','L1 + R -> tight','L1 + R + tight -> HO']
+
 		c = TCanvas('cL1AndTightL1Count','L1AndTightL1Count')
 		
-		h = TH1D('hL1AndTightL1Count','L1 Efficiency',4,-0.5,N_BINS - .5)
+		h = TH1D('hL1AndTightL1Count','L1 Cutflow',4,-0.5,N_BINS - .5)
+		hInverted = TH1D('hL1AndTightL1CountInverted','L1 Efficiency',4,-0.5,N_BINS - .5)
+		hInverted.SetFillStyle(3002)
+		hInverted.SetFillColor(colorRwthOrange)
+		hInverted.SetLineColor(colorRwthOrange)
+		hInverted.SetLineStyle(3)
 		
-		hL13x3Alone = TH1D('hL1And3x3Alone','',1,.5,1.5)
-		hL13x3Alone.SetBinContent(1,nAll3x3/nAll)
-		hL13x3Alone.SetBinError(1,calcSigma(nAll3x3,nAll))
+		hL13x3Alone = TH1D('hL1And3x3Alone','',1,1.5,2.5)
+		hL13x3Alone.SetBinContent(1,nL1RecoHo/nL1Reco)
+		hL13x3Alone.SetBinError(1,calcSigma(nL1RecoHo,nL1Reco))
 		hL13x3Alone.SetLineColor(colorRwthMagenta)
 		
-		hTightL13x3Alone = TH1D('hTightL1And3x3Alone','',1,1.5,2.5)
-		hTightL13x3Alone.SetBinContent(1,nTight3x3/nTight)
-		hTightL13x3Alone.SetBinError(1,calcSigma(nTight3x3,nTight))
+		hTightL13x3Alone = TH1D('hTightL1And3x3Alone','',1,2.5,3.5)
+		hTightL13x3Alone.SetBinContent(1,nL1RecoHoTight/nL1RecoHo)
+		hTightL13x3Alone.SetBinError(1,calcSigma(nL1RecoHoTight,nL1RecoHo))
 		hTightL13x3Alone.SetLineColor(colorRwthTuerkis)
 		
 		for i in range(2,N_BINS + 1):
 			h.SetBinContent(i,binContents[i-1]/binContents[1])
 			h.GetXaxis().SetBinLabel(i,binLabels[i-1])
+			hInverted.SetBinContent(i,binContentsInverted[i-1]/binContentsInverted[1])
+			hInverted.GetXaxis().SetBinLabel(i,binLabelsInverted[i-1])
 			
+		h.GetXaxis().SetBinLabel(1,'L1')
 		h.SetBinContent(1,1)
+		hInverted.GetXaxis().SetBinLabel(1,'L1')
+		hInverted.SetBinContent(1,1)
 		
 		h.SetLineColor(colorRwthDarkBlue)
 		h.SetStats(0)
 		h.GetYaxis().SetTitle('rel. fraction')
 		h.Draw()
-		hL13x3Alone.Draw('same e')
-		hTightL13x3Alone.Draw('same e')
+#		hL13x3Alone.Draw('same e')
+#		hTightL13x3Alone.Draw('same e')
+		hInverted.Draw('same')
+		hInverted.GetXaxis().Draw('same')
 		
 		setupAxes(h)
 		
 		legend = getLegend(y2=.9,x1=.55)
-		legend.AddEntry(h,'Normed to total Events','l')
-		legend.AddEntry(hL13x3Alone,'3x3 matching normed to # L1','le')
-		legend.AddEntry(hTightL13x3Alone,'3x3 matching normed to # tight L1','le')
+		legend.AddEntry(h,'First match HO then use tight','l')
+#		legend.AddEntry(hL13x3Alone,'3x3 matching normed to # L1 + R','le')
+#		legend.AddEntry(hTightL13x3Alone,'Normed to # L1 + R + HO','l')
+		legend.AddEntry(hInverted,'Inverted order for HO and tight','f')
 		legend.Draw()
 		
 		label = self.drawLabel()
 		
-		self.commandLine.output('###############################################')
-		self.commandLine.output('n L1: \t\t%d \t=> %5.2f' 		% (nAll,nAll/float(nEvents)*100))
-		self.commandLine.output('n L1 3x3: \t\t%d \t=> %5.2f (%5.2f)' 	% (nAll3x3,nAll3x3/float(nEvents)*100,nAll3x3/float(nAll)*100))
-		self.commandLine.output('n tight L1: \t%d \t=> %5.2f'	% (nTight,nTight/float(nAll)*100))
-		self.commandLine.output('n tight L1 3x3: \t%d \t=> %5.2f (%5.2f)' % (nTight3x3,nTight3x3/float(nEvents)*100,nTight3x3/float(nTight)*100))
-		self.commandLine.output('###############################################')
-		
-		
+		textObjects = []
+
+		#for (Int_t i=1;i<=30;i++) t.DrawText(h->GetBinCenter(i),yt,Form("%d",i%10));
+		for i in range(1,4):
+			t = TText()
+			t.SetTextSize(0.025)
+			t.SetTextAlign(22)
+			t.SetTextColor(colorRwthOrange)
+			t.DrawTextNDC(getXinNDC(hInverted.GetBinCenter(i+1)),0.05,binLabelsInverted[i])
+#			Double_t yt = - h->GetMaximum()/15.;
+			textObjects.append(t)
 		c.Update()
 		c.SaveAs('plots/efficiency/l1AndTightL1Counters.gif')
 		
-		return h,c,hL13x3Alone,hTightL13x3Alone,label,legend
+		return h,c,hL13x3Alone,hTightL13x3Alone,label,legend,hInverted,textObjects
 	
 	def plotTightL1EtaPhiRatio(self):
 		gL1Tight = self.fileHandler.getGraph('hoMuonAnalyzer/graphs/L1TightMuons')
