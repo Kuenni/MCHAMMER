8acb2ca30299edaa51b43f8ddf266e002c93a756
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2016-05-12 13:38:30 +0200

    Add templates for Mu minus without B Field Production

---
+++ Production/noBFieldMuMinus/submit.py
@@ -0,0 +1,89 @@
+#! /usr/bin/env python2
+import cesubmit
+import os,sys,math
+import time
+import glob
+import subprocess
+import logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger(__name__)
+import ConfigParser
+import argparse
+
+class Submitter:
+    
+    def __init__(self,nJobs):
+        self.taskName = os.path.abspath(os.path.curdir).split('/')[-1]
+	print 'TaskName: ',self.taskName
+        self.cmsswVersion = 'CMSSW_7_4_15'
+        self.scramArch='slc6_amd64_gcc491'
+        self.gridPackName = 'muMinusNoBProduction.tar.gz'
+        self.nJobs = nJobs
+        self.additionalGridPackContents = []
+
+    def addGridPackContent(self,filename):
+        self.additionalGridPackContents.append(filename)
+    
+    def setGridPackName(self,gridPackName):
+        self.gridPackName = gridPackName
+        
+    def getStatus(self):
+	taskDir = '/user/kuensken/tapasTasks/'
+	for dir in os.listdir(taskDir):
+		if not dir.startswith(self.taskName):
+			continue
+		print '\ttaskDirs: ',dir
+	#	task=cesubmit.Task(dir,directory = '/user/kuensken/tapasTasks/' + dir,cmsswVersion=self.cmsswVersion, scramArch=self.scramArch)
+	#	task.getStatus()
+        
+    def submit(self):
+        print "Preparing task", self.taskName
+	if not os.path.exists('configs'):
+		os.mkdir('configs')        
+	#ls -hl
+        tempExe = '''
+    cmsRun $1
+    RET=$?
+    if [ $RET -eq 0 ]; then
+        uberftp grid-srm "cd /pnfs/physik.rwth-aachen.de/cms/store/user/akunsken/SingleMuMinusNoBField; put $2" 
+        echo "Finished execution";
+    else
+        exit $RET;
+    fi
+    '''
+        tempFile = open('exec.sh','w')
+        tempFile.write(tempExe)
+        tempFile.close()
+        
+        #uploadurl = cesubmit.createAndUploadGridPack(['./','./'],self.gridPackName)
+        #task.addGridPack(uploadurl,extractdir="$CMSSW_BASE/src/SimMuon")
+        for j in range(0, 25):
+#            output = './RunOutput' + str(j)
+            task=cesubmit.Task(self.taskName + str(j),directory = '/user/kuensken/tapasTasks/' + self.taskName + str(j),cmsswVersion=self.cmsswVersion, scramArch=self.scramArch)
+            task.executable=os.path.abspath('exec.sh')
+            #uploadurl = cesubmit.createAndUploadGridPack(['./GenFiles','./GenFiles'],self.gridPackName)
+            #task.addGridPack(uploadurl,extractdir="$CMSSW_BASE/src/SimMuon")
+            for i in range(1,self.nJobs+1): 
+                job=cesubmit.Job()
+		createRunConfig(j*self.nJobs+i)
+                job.arguments.append('runConfigProduction_' + str(j*self.nJobs+i) + '.py')
+                job.arguments.append('SingleMuPt1to200_noBField_' + str(j*self.nJobs+i) + '.root')
+                job.inputfiles = [os.path.abspath(os.path.curdir) + '/configs/runConfigProduction_' + str(j*self.nJobs+i) + '.py']
+#		job.outputfiles.append(output)
+                task.addJob(job)
+            task.submit(6)
+        print "[Done]"
+
+#create the cms run cfgs
+def createRunConfig(instance):
+	outfileName = 'configs/runConfigProduction_' + str(instance) + '.py'
+	#Use a dedicated cfg template if given
+	configTemplate = 'runConfigProduction_template.py'
+	with open(configTemplate) as infile:
+		with open(outfileName,'w') as outfile:
+			for line in infile.readlines():
+				line = line.replace('%INSTANCE%', str(instance))
+				outfile.write(line)
+		outfile.close()
+	infile.close()
+	print 'Created config', outfileName
