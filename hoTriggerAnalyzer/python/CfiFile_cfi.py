12cceccf461f6e3e0b2cfb484f3b65c6ee3abab5
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2016-06-30 16:07:37 +0200

    Create useful config fragment for ho muon analyzer

--- hoTriggerAnalyzer/python/CfiFile_cfi.py
+++ hoTriggerAnalyzer/python/CfiFile_cfi.py
@@ -1,4 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-demo = cms.EDAnalyzer('hoTriggerAnalyzer'
+hoTriggerAnalyzer = cms.EDAnalyzer('hoTriggerAnalyzer',
+	genSrc = cms.InputTag("genParticles"),
+    l1MuonSrc=cms.InputTag("l1extraParticles"),
+    horecoSrc = cms.InputTag("horeco"),
+    hltSumAODSrc = cms.InputTag("hltTriggerSummaryAOD"),
+    l1MuonGenMatchSrc = cms.InputTag("l1MuonGenMatch"),
+    hoEnergyThreshold = cms.double(0.2),
+	maxDeltaR = cms.double(0.3),
+	debug = cms.bool(True),
+	isData = cms.bool(False),
+	maxDeltaRL1MuonMatching = cms.double(1.),
+	TrackAssociatorParameters=parameters,
+	hoDigiSrc = cms.InputTag('simHcalDigis'),
+	hoAdcThreshold = cms.int32(60)		
 )
