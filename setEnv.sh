3a89ec731d25e92f6ed2225ebc6c229710a77f7b
Andreas Kuensken <andreas@kuensken-online.de>
2016-07-22 12:06:31 +0200

    Make setup script for HO Muon Trigger base working

--- setEnv.sh
+++ setEnv.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #Add the python directory to the path and the Pythonpath
-export HOMUONTRIGGER_BASE=$CMSSW_BASE/src/HoMuonTrigger
+BASEDIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+echo "$BASEDIR"
+export HOMUONTRIGGER_BASE=$BASEDIR/src/HoMuonTrigger
 export PATH=$HOMUONTRIGGER_BASE/python:$PATH
 export PYTHONPATH=$HOMUONTRIGGER_BASE/python:$PYTHONPATH
 
