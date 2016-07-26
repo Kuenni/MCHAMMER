d38be28b81f7487f62ca69e2c291f953d68b8887
Andreas Kuensken <andreas@kuensken-online.de>
2016-07-26 09:49:53 +0200

    Make setup script usable

--- setEnv.sh
+++ setEnv.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #Add the python directory to the path and the Pythonpath
 BASEDIR="$( cd "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
 echo "$BASEDIR"
-export HOMUONTRIGGER_BASE=$BASEDIR/src/HoMuonTrigger
+export HOMUONTRIGGER_BASE=$BASEDIR
 export PATH=$HOMUONTRIGGER_BASE/python:$PATH
 export PYTHONPATH=$HOMUONTRIGGER_BASE/python:$PYTHONPATH
 
