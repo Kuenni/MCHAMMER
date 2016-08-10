0616c04eb9b1df36ecccd8283cc5db0d859071b4
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2016-08-10 08:41:58 +0200

    Some debug output added

--- hoTriggerAnalyzer/plugins/hoMuonAnalyzer.cc
+++ hoTriggerAnalyzer/plugins/hoMuonAnalyzer.cc
@@ -1544,6 +1544,8 @@ const L1MuRegionalCand* hoMuonAnalyzer::findBestCandMatch(const l1extra::L1MuonP
 		for( dttfCand = dttfCands.begin(); dttfCand != dttfCands.end();	++dttfCand ) {
 			if(dttfCand->empty()) continue;
 			std::cout << "Finding best cand" << std::endl;
+			std::cout << dttfCand->etaValue() << std::endl;
+
 			float dR = deltaR(l1Muon->eta(),l1Muon->phi()+ L1PHI_OFFSET,dttfCand->etaValue(),dttfCand->phiValue());
 			if(dR < 0.1){
 				if(dR < bestDeltaR){
