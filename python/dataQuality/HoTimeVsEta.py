2b247a888c010bfef79e85fb54bf2c638e62b353
Andreas Kuensken <kuensken@physik.rwth-aachen.de>
2016-09-23 10:26:57 +0200

    Add abililty to add custom titles to plots Add plot for fraction in time window for HO and L1 together

--- python/dataQuality/HoTimeVsEta.py
+++ python/dataQuality/HoTimeVsEta.py
@@ -4,10 +4,13 @@ Created on Jul 25, 2016
 @author: kuensken
 '''
 from plotting.Plot import Plot
-from plotting.Utils import getMedian, calcPercent, fillGraphIn2DHist, calcSigma
-from ROOT import Double, TH1D, TCanvas, TEfficiency, TF1, TH2D, TBox
-from plotting.PlotStyle import setupAxes, colorRwthMagenta
+from plotting.Utils import getMedian, calcPercent, fillGraphIn2DHist, calcSigma,\
+	getLegend
+from ROOT import Double, TH1D, TCanvas, TEfficiency, TF1, TH2D, TBox, TGaxis,gPad,gStyle,TPaveText
+from plotting.PlotStyle import setupAxes, colorRwthMagenta, colorRwthRot,\
+	colorRwthDarkBlue, colorRwthDarkGray
 from math import fabs
+import numpy as np
 
 class HoTimeVsEta(Plot):
 	'''
@@ -87,9 +90,11 @@ class HoTimeVsEta(Plot):
 	### ========================
 	### Plots of HO time vs iEta
 	### ========================
-	def makeTimeVsEtaPlot(self,source):
+	def makeTimeVsEtaPlot(self,source,title = ""):
 		canvas = TCanvas(source)
-		hist = TH2D(source,source + ";i#eta;Time / ns;#",33,-16.5,16.5,
+		if title == "":
+			title = source
+		hist = TH2D(source,title + ";i#eta;Time / ns;#",33,-16.5,16.5,
 				201,-100.5,100.5)
 		graph = self.fileHandler.getGraph('graphs/timingSupport_' + source)
 		fillGraphIn2DHist(graph, hist)
@@ -130,7 +135,7 @@ class HoTimeVsEta(Plot):
 	def makeL1TimeVsEtaPlot(self,source):
 		canvas = TCanvas(source,source)
 		canvas.SetLogz()
-		hist = TH2D(source,source + ";#eta_{L1};BXID;#",10,-1,1,
+		hist = TH2D(source,source + ";#eta_{L1};BXID;#",20,-1,1,
 				7,-3.5,3.5)
 		graph = self.fileHandler.getGraph('graphs/timingSupport_' + source)
 		fillGraphIn2DHist(graph, hist)
@@ -141,25 +146,138 @@ class HoTimeVsEta(Plot):
 		label = self.drawLabel()
 		canvas.Update()
 		return canvas,label,hist
+
+	### ============================================
+	### Plot fraction vs. Eta for HO and L1 together
+	### ============================================
+	def makeCombinedEtaPlot(self, tight = False):
+		hist = self.makeL1TimeVsEtaPlot(('tight_' if tight else '') + 'dtOnly_bxidVsEta')[2]
+		countsInL1 = []
+		for x in np.arange(-.95,1.05,0.1):
+			totalCounter = 0
+			zeroCount = 0
+			for y in range(-2,3):
+				totalCounter += hist.GetBinContent(hist.FindBin(x,y))
+				if y == 0:
+					zeroCount = hist.GetBinContent(hist.FindBin(x,y))
+			countsInL1.append({'total':totalCounter,'zero':zeroCount,'eta':x})
+	
+		#Graph for results
+		graph1 = TEfficiency(hist.GetName(),"",8,-9.195,-.5)
+		graph2 = TEfficiency(hist.GetName(),"",8,.5,9.195)
+	
+		for item in countsInL1:
+			if item['total'] == 0:
+				continue
+			print item['total'],item['zero'],item['eta']
+			if item['eta'] < 0:
+				graph1.SetTotalEvents(graph1.FindFixBin(-0.5 + item['eta']/0.087),int(item['total']))
+				graph1.SetPassedEvents(graph1.FindFixBin(-0.5 + item['eta']/0.087),int(item['zero']))
+			else:
+				graph2.SetTotalEvents(graph2.FindFixBin(0.5 + item['eta']/0.087),int(item['total']))
+				graph2.SetPassedEvents(graph2.FindFixBin(0.5 + item['eta']/0.087),int(item['zero']))
+		
+		histHo = None
+		if tight:
+			histHo = self.plotTightHoTimeVsEta()[3][1]
+		else:
+			histHo = self.plotHoTimeVsEta()[3][1]
+			
+		histHo.SetTitle(('Tight ' if tight else '') + 'Unmatched DT, + HO')
+		
+		canvas = TCanvas('combinedPlot' + ('Tight ' if tight else '') + hist.GetName(),'combinedPlot')
+		canvas.cd().SetTopMargin(.15)
+		histHo.Draw('ap')
+		canvas.Update()
+		canvas.cd().SetTicks(0,0)
+		
+		histHo.SetMarkerStyle(2)
+		histHo.SetLineColor(colorRwthDarkBlue)
+		histHo.SetMarkerColor(colorRwthDarkBlue)
+		histHo.GetPaintedGraph().GetXaxis().SetRangeUser(-12,12)
+		histHo.GetPaintedGraph().GetXaxis().SetLabelColor(colorRwthDarkBlue)
+		histHo.GetPaintedGraph().GetXaxis().SetTitleColor(colorRwthDarkBlue)
+		histHo.GetPaintedGraph().GetXaxis().SetAxisColor(colorRwthDarkBlue)
+		yMax = gPad.GetFrame().GetY2()
+		yMin = gPad.GetFrame().GetY1()
+		
+		#Left axis part
+		f1 = TF1("f1","x",-0.87,0)
+		A1 = TGaxis(-10,yMax,-0.5,yMax,"f1",010,"-")
+		A1.SetLineColor(colorRwthRot)
+		A1.SetLabelColor(colorRwthRot)
+		A1.Draw()
+		
+		#Right axis part
+		f2 = TF1("f2","x",0,0.87)
+		A2 = TGaxis(0.5,yMax,10,yMax,"f2",010,"-")
+		A2.SetLineColor(colorRwthRot)
+		A2.SetLabelColor(colorRwthRot)
+		A2.Draw()
+		
+		#Box for shading out 0
+		box = TBox(-.5,yMin,0.5, yMax)
+		box.SetLineColor(colorRwthDarkGray)
+		box.SetFillColor(colorRwthDarkGray)
+		box.SetFillStyle(3013)
+		box.Draw('same')
+		
+		#Left L1 eta
+		graph1.SetMarkerColor(colorRwthRot)
+		graph1.SetLineColor(colorRwthRot)
+		graph1.SetMarkerStyle(20)
+		graph1.Draw('same,p')
+		
+		#Right L1Eta
+		graph2.SetMarkerColor(colorRwthRot)
+		graph2.SetLineColor(colorRwthRot)
+		graph2.SetMarkerStyle(20)
+		graph2.Draw('same,p')
+		
+		#Label for extra axis
+		axisLabel = TPaveText( 0.83,0.85,0.89,0.9,"NDC")
+		axisLabel.AddText('#eta_{L1}')
+		axisLabel.SetBorderSize(0)
+		axisLabel.SetFillStyle(0)
+		axisLabel.SetTextColor(colorRwthRot)
+		axisLabel.SetTextSize(0.05)
+		axisLabel.Draw()
+		
+		#Legend
+		legend = getLegend(x1=0.1,y1=0.1,x2=0.4,y2=.35)
+		legend.AddEntry(histHo,'HO #in #pm12.5 ns','pe')
+		legend.AddEntry(graph1,('Tight ' if tight else '') + 'L1 BXID = 0','pe')
+		legend.Draw()
+		
+		canvas.Update()
+		self.storeCanvas(canvas, "combinedFractionL1AndHo" + ('Tight ' if tight else ''),drawMark=False)
+			
+		return histHo, graph1,canvas,A1,f1,A2,f2,box,graph2,axisLabel,legend
 	
 	### ============================
 	### Predefined plotter functions
 	### ============================
 	
 	def plotHoTimeVsEta(self):
-		canvas, hist, label, graph, boxes, tofFunction = self.makeTimeVsEtaPlot('UnmatchedDtHoTimeGraph')
+		canvas, hist, label, graph, boxes, tofFunction = self.makeTimeVsEtaPlot('UnmatchedDtHoTimeGraph', "Unmatched DT")
 		self.storeCanvas(canvas,'UnmatchedDtHo_TimeVsEta')
 		fractionsPerEtaData = self.plotFractionsVsEta(graph,"Fraction of HORecHits at BXID 0",'UnmatchedDtHoTimeGraph_fractionVsEta')
 		return canvas,hist,label,fractionsPerEtaData, boxes, tofFunction
 	
 	def plotHoTimeVsEtaBxWrong(self):
-		canvas, hist, label, graph, boxes, tofFunction = self.makeTimeVsEtaPlot('UnmatchedDtHoBxNot0TimeGraph')
+		canvas, hist, label, graph, boxes, tofFunction = self.makeTimeVsEtaPlot('UnmatchedDtHoBxNot0TimeGraph',"Unmatched DT BX Wrong")
 		self.storeCanvas(canvas,'UnmatchedDtHoBxNot0TimeGraph_TimeVsEta')
 		fractionsPerEtaData = self.plotFractionsVsEta(graph,"Fraction of HORecHits at BXID 0,L1 BX wrong",'UnmatchedDtHoBxNot0TimeGraph_fractionVsEta')
 		return canvas,hist,label,fractionsPerEtaData, boxes, tofFunction
+	
+	def plotTightHoTimeVsEta(self):
+		canvas, hist, label, graph, boxes, tofFunction = self.makeTimeVsEtaPlot('tight_UnmatchedDtHoTimeGraph',"Tight unmatched DT")
+		self.storeCanvas(canvas,'tight_UnmatchedDtHoTimeGraph_TimeVsEta')
+		fractionsPerEtaData = self.plotFractionsVsEta(graph,"Fraction of HORecHits at BXID 0,Tight L1",'tight_UnmatchedDtHoTimeGraph_fractionVsEta')
+		return canvas,hist,label,fractionsPerEtaData, boxes, tofFunction
 		
 	def plotTightHoTimeVsEtaBxWrong(self):
-		canvas, hist, label, graph, boxes, tofFunction = self.makeTimeVsEtaPlot('tight_UnmatchedDtHoBxNot0TimeGraph')
+		canvas, hist, label, graph, boxes, tofFunction = self.makeTimeVsEtaPlot('tight_UnmatchedDtHoBxNot0TimeGraph',"Tight unmatched DT BX Wrong")
 		self.storeCanvas(canvas,'tight_UnmatchedDtHoBxNot0TimeGraph_TimeVsEta')
 		fractionsPerEtaData = self.plotFractionsVsEta(graph,"Fraction of HORecHits at BXID 0,Tight L1 BX wrong",'tight_UnmatchedDtHoBxNot0TimeGraph_fractionVsEta')
 		return canvas,hist,label,fractionsPerEtaData, boxes, tofFunction
@@ -174,3 +292,9 @@ class HoTimeVsEta(Plot):
 		canvas, hist, label = self.makeL1TimeVsEtaPlot('bxidVsEta')
 		self.storeCanvas(canvas,'bxidVsEta')
 		return canvas,hist,label
+	
+	def plotCombined(self):
+		return self.makeCombinedEtaPlot()
+	
+	def plotCombinedTight(self):
+		return self.makeCombinedEtaPlot(True)
